490d52f0e02a114def602deff635c08d89ac7139
ReverseCall <r.alisson123987@gmail.com>
2026-04-05 19:57:49 -0300

    Pagina html quase pronta.

--- SiteDoMono/settings.py
+++ SiteDoMono/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 MODERATION_PASSWORD = os.getenv('ADM_PASS')
 # SECURITY WARNING: don't run with debug turned on in production!
-ATUALIZANDO = False
+ATUALIZANDO = True
 DEBUG = ATUALIZANDO
 
 
